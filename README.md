b830bacaa460b79392496ee47ddfe3531467d15e
Chris Shiplet <chris@nearengine.com>
2013-09-21 14:45:22 -0700

    First proof of concept and documentation

---
+++ README.md
@@ -0,0 +1,19 @@
+# smartstart.py
+
+A hacky proof of concept in Python showing lax security in DEI brand (Viper, Clifford, Python, etc) SmartStart remote start systems, and possibly other vendors utilizing Cal Amp's asset monitoring API.
+
+All API requests are made via HTTPS, but all endpoints use GET parameters. Username and password are sent in the clear from mobile devices over wifi or cellular data. Additionally, no noops are present and session IDs are also sent via parameter, so session hijacking and packet replay attacks are possible even if the username and password was not able to be sniffed.
+
+## Usage
+
+Assuming Python is installed, this script can be installed as follows:
+
+    git clone git://github.com/nearengine/smartstart.py.git && cd smartstart.py
+    mv _login.json login.json
+    chmod +x smartstart.py
+    
+Then add your SmartStart credentials to `login.json` and:
+
+    ./smartstart.py <command> [<device>]
+
+You can omit the arguments for a list of commands. Windows users, you're on your own ;)
--- smartstart.py
+++ smartstart.py
@@ -3,6 +3,95 @@
 import time, sys, json
 import requests
 
-s = requests.session()
-r = s.get('https://colt.calamp-ts.com/auth/login/user/pass')
+man = '''
+Usage:
+smartstart.py <command> [<device>]
 
+Commands:
+arm    - locks and arms the vehicle
+disarm - unlocks and disarms the vehicle
+trunk  - opens the trunk, if equipped
+panic  - starts the alarm
+remote - starts the engine
+
+Devices:
+An integer of range 0-? (use if you have multiple devices on your account)
+'''
+
+# API parameters
+url_base = 'https://colt.calamp-ts.com/'
+commands = ['arm', 'disarm', 'panic', 'trunk', 'remote']
+
+# Check for at least one command line argument, otherwise print the help screen
+if len(sys.argv) < 2:
+    print man
+    sys.exit()
+
+# Make sure <command> is present in the list of available API endpoints above,
+# otherwise print the help screen
+if sys.argv[1] in commands:
+    command = sys.argv[1]
+else:
+    print man
+    sys.exit()
+
+# This checks if a <device> is present in the arguments. If it's a number,
+# it uses it as the device selector, in case if you have multiple devices on
+# your account. Otherwise, it defaults to 0
+try:
+    sys.argv[2]
+except:
+    dev_sel = 0
+else:
+    if sys.argv[2].isdigit():
+        dev_sel = sys.argv[2]
+    else:
+        dev_sel = 0
+
+# Load username & password from config file as JSON
+try:
+    user = open('login.json')
+    user = json.load(user)
+except:
+    print 'error parsing login.json'
+    sys.exit()
+
+# Get a session ID
+s = requests.session()
+r = s.get(url_base + 'auth/login/' + user["username"] + '/' + user["password"])
+
+try:
+    sess_res = json.loads(r.text)
+    sess_id = sess_res["Return"]["Results"]["SessionID"]
+except:
+    print 'error getting session ID, incorrect user/pass?'
+    sys.exit()
+
+# Get list of vehicles
+r = s.get(url_base
+    + 'device/advancedsearch/null/100/0/ActiveWithAsset/null/false?sessid='
+    + sess_id)
+
+try:
+    device_info = json.loads(r.text)
+    device_id = device_info["Return"]["Results"]["Devices"][dev_sel]["DeviceId"]
+    # print device_info["Return"]["Results"]["Devices"][dev_sel]["GeoPosition"]
+except:
+    print 'error getting device ID'
+    sys.exit()
+
+# Carry out command
+r = s.get(url_base
+    + 'device/sendcommand/'
+    + device_id
+    + '/'
+    + command
+    + '?sessid='
+    + sess_id)
+
+try:
+    res = json.loads(r.text)
+    print json.dumps(res, indent=4)
+except:
+    print 'error parsing response json'
+    sys.exit()
